1448ea967bbe9576ccb4b8fe19e9c81b8ab9fbed
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2024-10-27 22:18:25 +0200

    'updated'

--- server_config/config.php
+++ server_config/config.php
@@ -1,5 +1,8 @@
 <?php
 
-    $conn = mysqli_connect("localhost", "root", "herotel")
+    $conn = mysqli_connect("localhost", "root", "herotel") or die(mysqli_error($conn));
     session_start();
+    // if(isset($_SESSION[""]) && $_SESSION[""] {
+
+    // }
 ?>
