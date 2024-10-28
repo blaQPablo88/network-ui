c6acd862659c2a7bceb7f6996ffbc72602784659
thembalakhe <thmalinjhb024@student.wethinkcode.co.za>
2024-10-28 16:36:27 +0200

    updated

--- index.php
+++ index.php
@@ -1,14 +1,33 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
     <title>Document</title>
 </head>
 <body>
+    <h1>Router</h1>
+
+    <h2>Login</h2>
+
+    <form action="">
+
+        <div class="container">
+            <label for=""">Email</label>
+            <input type="email" name=s"" id="">
+            <label for="">Pasword</label>
+            <input type="text" name="" id="">
+            <button type="submit">Login</button>
+        </div>
+
+    </form>
+
+    <div class="socials">
+        <p>Facebook</p>
+        <p>Twitter</p>
+        <p>WhatsAppp</p>
+    </div>
     
 </body>
 </html>
-
