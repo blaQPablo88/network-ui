2854e7cdd6328f07ec17d0ca221159ab311f5422
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2024-10-31 11:08:32 +0200

    updated sql file

---
+++ server_config/app_db.sql
@@ -0,0 +1,35 @@
+-- `user_info` table:
+CREATE TABLE IF NOT EXISTS `user_info` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `f_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
+
+-- insert into `user_info` table:
+INSERT INTO `user_info` (`f_name`, `email`, `password`) VALUES
+('Kagiso', 'kamogotjhb024@students.wethinkcode.co.za', '565CFDGhgs1'),
+('Kagiso', 'kamogotjhb024@students.wethinkcode.co.za', '565CFDGhgs1');
+('Kagiso', 'kamogotjhb024@students.wethinkcode.co.za', '565CFDGhgs1');
+('Kagiso', 'kamogotjhb024@students.wethinkcode.co.za', '565CFDGhgs1');
+
+
+-- `basic_status` table:
+
+
+
+-- `device_info` table:
+
+
+
+-- `device_management` table:
+CREATE TABLE IF NOT EXISTS `device_management`:
+    `id_device_management`
+    `username`
+    `password`
+-- `router_indox` table:
+
+
+
+-- `wifi` table:
